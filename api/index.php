2e8f087fad9d8c24d2ed8b6bc98d5e16f26b6a65
habib <habibsuprayoga325@gmail.com>
2026-06-28 20:24:17 +0700

    Override SCRIPT_NAME in api/index.php to fix Vercel folder routing

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 
+// Override script name to prevent CodeIgniter 4 from stripping the "/api" folder prefix from URIs on Vercel
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+
 // Forward request to the CodeIgniter 4 public index.php entry point
 require __DIR__ . '/../backend-api/public/index.php';
